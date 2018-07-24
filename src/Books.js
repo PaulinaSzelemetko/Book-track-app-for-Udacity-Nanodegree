fd535d2895ee4f216a53de2be6c9f8618a980423
Paulina Szelemetko <p.szelemetko@gmail.com>
2018-07-24 14:23:12 +0200

    correct some tags in books js

--- src/Books.js
+++ src/Books.js
@@ -10,8 +10,8 @@ class Books extends React.Component {
                     <li key={book.id} className='book-list-item'>
                         <div className="book">
                             <div className="book-top">
-                                 <div className="book-cover" style={{ width: 128, height: 174, backgroundImage: 'url("http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api")' }}></div>
-                                     <div className="book-shelf-changer">
+                                <div className="book-cover" style={{ width: 128, height: 174, backgroundImage: 'url("http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api")' }}></div>
+                                <div className="book-shelf-changer">
                                     <select>
                                      <option value="move" disabled>Move to...</option>
                                      <option value="currentlyReading">Currently Reading</option>
@@ -20,10 +20,10 @@ class Books extends React.Component {
                                      <option value="none">None</option>
                                      </select>
                                  </div>
-                              </div>
-                             <div className="book-title">Oh, the Places Youll Go!</div>
-                             <div className="book-authors">Seuss</div>
-                             </div>
+                            </div>
+                            <div className="book-title">Oh, the Places Youll Go!</div>
+                            <div className="book-authors">Seuss</div>
+                        </div>
                     </li>
             </ol>
         )
