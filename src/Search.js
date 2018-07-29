c8d47de2538eae2013e8a5b9c93f7e006794888c
Paulina Szelemetko <p.szelemetko@gmail.com>
2018-07-29 22:59:44 +0200

    add search js

--- src/Search.js
+++ src/Search.js
@@ -2,23 +2,53 @@ import React, { Component } from 'react'
 import './App.css'
 import { Link } from "react-router-dom";
 import Book from './Book'
+import * as BooksAPI from './BooksAPI'
 
+class Search extends React.Component {
+  state = {
+    books: []
+  }
+  
+  updateQuery = (query) => {
+    let books = [];
+    if(query === "") {
+      this.setState({books: []})
+    } else {
+      BooksAPI.search(query).then((data) => {
+        if(data.error) {
+          this.setState({ books: [] });
+        } else {
+          books = books.concat(data);
+          this.setState({ books: books });
+        }
+      })
+    }
+  }
 
-const search = (props) => {
+  render() {
+    return (   
 
-    const book = props.book;
-
-    return (    
-
-        <div className="search-books">
-                <div className="search-books-bar">
-                    <Link className="close-search" to="/">Close</Link>
-                    <div className="search-books-input-wrapper">
-                          <input type="text" placeholder="Search books..."/>
-                    </div>
-                </div>
+    <div className="search">
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link className="close-search" to="/">Close</Link>
+          <div className="search-books-input-wrapper">
+            <input type="text" placeholder="Search books..." onChange={(event) => this.updateQuery(event.target.value)}/>
+          </div>
         </div>
-    )}
+      </div>
+      <div className="results">
+        <ol className="books-grid">
+          {this.state.books.map((book) => (
+            <li>
+              <Book book={book} changeShelf={this.props.changeShelf}/>
+            </li>
+          ))}
+        </ol>
+      </div>
+    </div>
+  )}
+}
 
 
-export default search
+export default Search
